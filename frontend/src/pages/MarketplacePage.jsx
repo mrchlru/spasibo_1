3f11a02715936e15b9dace6004313f383dd97726
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-07-17 01:49:17 +0300

    Create MarketplacePage.jsx

---
+++ frontend/src/pages/MarketplacePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MarketplacePage() {
+  return <div style={{ padding: '20px' }}><h1>Магазин</h1><p>Эта страница находится в разработке.</p></div>;
+}
+
+export default MarketplacePage;
