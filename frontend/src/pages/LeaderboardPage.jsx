1d4e2c8a8d0f84862ff5e5ecb22552fe8d042360
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-07-17 01:48:43 +0300

    Create LeaderboardPage.jsx

---
+++ frontend/src/pages/LeaderboardPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LeaderboardPage() {
+  return <div style={{ padding: '20px' }}><h1>Рейтинг</h1><p>Эта страница находится в разработке.</p></div>;
+}
+
+export default LeaderboardPage;
