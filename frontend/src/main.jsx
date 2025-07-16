c189545d5eacded34065768ca63fb8a6aeb419dc
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-07-17 02:10:23 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
+// frontend/src/main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Убедитесь, что этот путь правильный
+import App from './App.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
