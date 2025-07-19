60cb6f205b9f6a037ed30df48282352631cede57
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-07-19 12:50:07 +0300

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .database import engine
+from .models import Base
+from .routers import users, transactions, market, admin
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+app.include_router(users.router)
+app.include_router(transactions.router)
+app.include_router(market.router)
+app.include_router(admin.router)
