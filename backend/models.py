8cc1a346cebf80b85f954e2401b03c66a2df43ac
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-07-20 12:37:31 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, BigInteger # <-- Добавили BigInteger
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, BigInteger, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from datetime import datetime
 
