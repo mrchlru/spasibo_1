751036e9b9af733c80cdc8049515e6f1234d2afc
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-08-16 01:59:24 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Убедитесь, что этот путь правильный
-import App from './App.jsx' 
+import App from './App.jsx'
+import './index.css' // <-- ДОБАВЛЯЕМ ИМПОРТ ГЛОБАЛЬНЫХ СТИЛЕЙ
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
