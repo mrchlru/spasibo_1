cd66c46418e5c7e6205881fead2c64634d74896e
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-09-04 22:40:39 +0300

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -46,6 +46,7 @@ class UserResponse(UserBase):
     status: Optional[str] = 'approved' 
     ticket_parts: int
     tickets: int
+    card_barcode: Optional[str] = None
     phone_number: Optional[str] = None
     date_of_birth: Optional[date] = None
     # purchases: List[PurchaseForUserResponse] = [] # Раскомментируйте, если понадобится
