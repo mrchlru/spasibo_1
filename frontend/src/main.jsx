18e9748e5906953990285c4d3fa5fcec08c2c2d2
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-09-17 16:58:42 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // <-- ДОБАВЛЯЕМ ИМПОРТ ГЛОБАЛЬНЫХ СТИЛЕЙ
+import { ModalAlertProvider } from './contexts/ModalAlertContext.jsx'; // 1. Импортируем
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2. Оборачиваем App в наш Provider */}
+    <ModalAlertProvider>
+      <App />
+    </ModalAlertProvider>
   </React.StrictMode>,
-)
+);
