3f11c9d15bf80d27115ebfef81fef6cd607c5097
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-09-17 19:56:29 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,21 @@
 // frontend/src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <-- ДОБАВЛЯЕМ ИМПОРТ ГЛОБАЛЬНЫХ СТИЛЕЙ
-import { ModalAlertProvider } from './contexts/ModalAlertContext.jsx'; // 1. Импортируем
-import { ConfirmationProvider } from './contexts/ConfirmationContext.jsx'; // 1. Импортируем
+// 1. Импортируем наши ПРАВИЛЬНЫЕ Provider'ы
+import { ModalAlertProvider } from './contexts/ModalAlertContext.jsx';
+import { ConfirmationProvider } from './contexts/ConfirmationContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NotificationProvider>
-      {/* 2. Оборачиваем App в еще один Provider */}
-      <ConfirmationProvider>
+    {/* 2. Оборачиваем App в правильные Provider'ы */}
+    {/* Теперь они действительно будут работать для всего приложения */}
+    <ConfirmationProvider>
+      <ModalAlertProvider>
         <App />
-      </ConfirmationProvider>
-    </NotificationProvider>
+      </ModalAlertProvider>
+    </ConfirmationProvider>
   </React.StrictMode>,
 );
