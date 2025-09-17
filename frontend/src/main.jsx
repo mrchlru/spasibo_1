4262c404b4b79c27e48ecc27f45390d2c01e8390
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-09-17 19:35:12 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // <-- ДОБАВЛЯЕМ ИМПОРТ ГЛОБАЛЬНЫХ СТИЛЕЙ
 import { ModalAlertProvider } from './contexts/ModalAlertContext.jsx'; // 1. Импортируем
+import { ConfirmationProvider } from './contexts/ConfirmationContext.jsx'; // 1. Импортируем
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. Оборачиваем App в наш Provider */}
-    <ModalAlertProvider>
-      <App />
-    </ModalAlertProvider>
+    <NotificationProvider>
+      {/* 2. Оборачиваем App в еще один Provider */}
+      <ConfirmationProvider>
+        <App />
+      </ConfirmationProvider>
+    </NotificationProvider>
   </React.StrictMode>,
 );
