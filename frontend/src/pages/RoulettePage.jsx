9bfd03faff396a35e09cdf0dce76538706534fdb
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-09-23 22:50:08 +0300

    Update RoulettePage.jsx

--- frontend/src/pages/RoulettePage.jsx
+++ frontend/src/pages/RoulettePage.jsx
@@ -7,22 +7,16 @@ import styles from './RoulettePage.module.css';
 import { FaInfoCircle } from 'react-icons/fa';
 import { useModalAlert } from '../contexts/ModalAlertContext';
 
-// --- Иконки для барабанов ---
-const thankYouIcon = "https://i.postimg.cc/cLCwXyrL/Frame-2131328056.webp"; // "Спасибо"
-const ticketIcon = "https://i.postimg.cc/pX05sN69/ticket-icon.png"; // Билет
-const jackpotIcon = "https://i.postimg.cc/W3B9pG1c/jackpot-icon.png"; // Джекпот
+const thankYouIcon = "https://i.postimg.cc/cLCwXyrL/Frame-2131328056.webp";
+const ticketIcon = "https://i.postimg.cc/pX05sN69/ticket-icon.png";
+const jackpotIcon = "https://i.postimg.cc/W3B9pG1c/jackpot-icon.png";
 
-// --- Призы и их иконки ---
 const PRIZES = {
-    // Маленькие призы
     1: thankYouIcon, 2: thankYouIcon, 3: thankYouIcon, 4: thankYouIcon, 5: thankYouIcon,
-    // Средние призы
     6: ticketIcon, 8: ticketIcon, 9: ticketIcon, 10: ticketIcon, 11: ticketIcon, 12: ticketIcon, 13: ticketIcon, 14: ticketIcon, 15: ticketIcon,
-    // Крупные призы
     16: jackpotIcon, 17: jackpotIcon, 18: jackpotIcon, 19: jackpotIcon, 20: jackpotIcon, 21: jackpotIcon, 22: jackpotIcon, 23: jackpotIcon, 24: jackpotIcon, 25: jackpotIcon, 26: jackpotIcon, 27: jackpotIcon, 28: jackpotIcon, 29: jackpotIcon, 30: jackpotIcon
 };
 
-// Все возможные иконки для анимации
 const reelSymbols = [thankYouIcon, ticketIcon, jackpotIcon, thankYouIcon, ticketIcon];
 
 function RoulettePage({ user, onUpdateUser }) {
@@ -133,21 +127,24 @@ function RoulettePage({ user, onUpdateUser }) {
                 </div>
             </div>
 
-            {/* --- СТРУКТУРА СЛОТ-МАШИНЫ --- */}
+            {/* --- ОБНОВЛЕННАЯ СТРУКТУРА СЛОТ-МАШИНЫ --- */}
             <div className={styles.slotMachineWrapper}>
                 <div className={styles.slotMachine}>
-                    <div className={styles.reelsContainer}>
-                        {[reel1Ref, reel2Ref, reel3Ref].map((ref, i) => (
-                            <div key={i} className={styles.reel}>
-                                <div className={styles.reelTrack} ref={ref}>
-                                    {[...reelSymbols, ...reelSymbols].map((symbol, index) => (
-                                        <div key={index} className={styles.symbol}>
-                                            <img src={symbol} alt="symbol" />
-                                        </div>
-                                    ))}
+                    <div className={styles.slotGloss}></div>
+                    <div className={styles.slotScreen}>
+                        <div className={styles.reelsContainer}>
+                            {[reel1Ref, reel2Ref, reel3Ref].map((ref, i) => (
+                                <div key={i} className={styles.reel}>
+                                    <div className={styles.reelTrack} ref={ref}>
+                                        {[...reelSymbols, ...reelSymbols].map((symbol, index) => (
+                                            <div key={index} className={styles.symbol}>
+                                                <img src={symbol} alt="symbol" />
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
                 <button className={styles.spinButton} onClick={handleSpin} disabled={isSpinning || localUser.tickets < 1}>
