87df1c64ad936265326b71f3164bf35a95628b41
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-10-01 22:39:56 +0300

    Create UserEngagementPage.jsx

---
+++ frontend/src/pages/admin/stats/UserEngagementPage.jsx
@@ -0,0 +1,14 @@
+// frontend/src/pages/admin/stats/UserEngagementPage.jsx
+import React from 'react';
+import PageLayout from '../../../components/PageLayout';
+
+const UserEngagementPage = () => {
+  return (
+    <div>
+      <h2>Лидеры вовлеченности</h2>
+      <p>Здесь будут списки лидеров по отправленным и полученным "спасибо".</p>
+    </div>
+  );
+};
+
+export default UserEngagementPage;
