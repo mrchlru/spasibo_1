9178425d29288358a4045cd0d1de005164138382
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-10-01 22:41:00 +0300

    Create InactiveUsersPage.jsx

---
+++ frontend/src/pages/admin/stats/InactiveUsersPage.jsx
@@ -0,0 +1,14 @@
+// frontend/src/pages/admin/stats/InactiveUsersPage.jsx
+import React from 'react';
+import PageLayout from '../../../components/PageLayout';
+
+const InactiveUsersPage = () => {
+  return (
+    <div>
+      <h2>Неактивные пользователи</h2>
+      <p>Здесь будет список пользователей, не совершивших ни одной транзакции.</p>
+    </div>
+  );
+};
+
+export default InactiveUsersPage;
