d14dd72bd22e89f570eaa78cff46b628f8876fe2
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-10-12 18:12:12 +0300

    Update crud.py

--- backend/crud.py
+++ backend/crud.py
@@ -332,7 +332,7 @@ async def create_market_item(db: AsyncSession, item: schemas.MarketItemCreate):
 async def create_purchase(db: AsyncSession, pr: schemas.PurchaseRequest):
     item = await db.get(models.MarketItem, pr.item_id)
     result = await db.execute(
-        select(models.User).where(models.User.telegram_id == request.user_id)
+        select(models.User).where(models.User.telegram_id == request.user_id))
 
     if not item or not user:
         raise ValueError("Товар или пользователь не найдены")
