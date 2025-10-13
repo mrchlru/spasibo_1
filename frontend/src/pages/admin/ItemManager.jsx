3ff34735cdedaca680d19cf63cb8ee3a12ced6a6
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-10-13 12:54:22 +0300

    Update ItemManager.jsx

--- frontend/src/pages/admin/ItemManager.jsx
+++ frontend/src/pages/admin/ItemManager.jsx
@@ -51,12 +51,12 @@ function ItemManager() {
   
   const calculatedPrice = useMemo(() => {
       if (!form.price_rub || form.price_rub <= 0) return 0;
-      return Math.round(form.price_rub / 50);
+      return Math.round(form.price_rub / 30);
   }, [form.price_rub]);
 
   const calculatedOriginalPrice = useMemo(() => {
     if (!form.original_price_rub || form.original_price_rub <= 0) return 0;
-    return Math.round(form.original_price_rub / 50);
+    return Math.round(form.original_price_rub / 30);
   }, [form.original_price_rub]);
 
   const forecast = useMemo(() => {
